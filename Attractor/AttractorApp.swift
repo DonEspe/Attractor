db43641c5c9281948493c3c9fc8fe9c8078450de
Don Espe <Don.Espe@gmail.com>
2024-02-07 19:38:43 -0600

    Initial Commit

---
+++ Attractor/AttractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttractorApp.swift
+//  Attractor
+//
+//  Created by Don Espe on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AttractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
